fcd40d950e45b9ee0231c95ac42599a415d18813
Issei Naruta <naruta@cookpad.com>
2020-06-15 11:47:14 +0900

    Don't load unused scripts

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -5,42 +5,38 @@ const srcDir = "../src/";
 
 module.exports = {
   entry: {
-    popup: path.join(__dirname, srcDir + "popup.ts"),
-    options: path.join(__dirname, srcDir + "options.ts"),
-    background: path.join(__dirname, srcDir + "background.ts"),
-    content_script: path.join(__dirname, srcDir + "content_script.ts"),
     amazon_shortener: path.join(__dirname, srcDir + "amazon_shortener.ts"),
     remove_query_strings: path.join(
       __dirname,
       srcDir + "remove_query_strings.ts"
     ),
-    zoom_closer: path.join(__dirname, srcDir + "zoom_closer.ts")
+    zoom_closer: path.join(__dirname, srcDir + "zoom_closer.ts"),
   },
   output: {
     path: path.join(__dirname, "../dist/js"),
-    filename: "[name].js"
+    filename: "[name].js",
   },
   optimization: {
     splitChunks: {
       name: "vendor",
-      chunks: "initial"
-    }
+      chunks: "initial",
+    },
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: [".ts", ".tsx", ".js"],
   },
   plugins: [
     // exclude locale files in moment
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new CopyPlugin([{ from: ".", to: "../" }], { context: "public" })
-  ]
+    new CopyPlugin([{ from: ".", to: "../" }], { context: "public" }),
+  ],
 };
